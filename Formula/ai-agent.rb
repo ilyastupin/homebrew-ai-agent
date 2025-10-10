57625b827681464de69737ba31bfba7778e4cb63
Ilya Stupin <ilyastupin@marcatopartners.com>
2025-10-09 23:50:48 -0700

    updates

--- Formula/ai-agent.rb
+++ Formula/ai-agent.rb
@@ -3,7 +3,7 @@ class AiAgent < Formula
   homepage "https://ai.inpoint.dev/local-agent"
   version "2.1.1"
   url "https://raw.githubusercontent.com/ilyastupin/homebrew-ai-agent/main/ai-agent-2.1.1.tar.gz"
-  sha256 "468c27fdcfbdd6f6b5006f1c7c23b41bd7ab30f2f68a4c7db23078dbe8263c93"
+  sha256 "404ac8d4ea1e9b7dbb5c7d5cf279a381a3ebd14cd542ee4612751089895d9e2c"
   license :cannot_represent
 
   def install
